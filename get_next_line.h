0a9cc498e9876cdfbf0b6b0ba835f80edb5a9522
diamorim <diamorim02@gmail.com>
2026-08-05 04:26:36 +0200

    norminette

--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damorim- <damorim-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/05 04:21:52 by damorim-          #+#    #+#             */
+/*   Updated: 2026/08/05 04:25:50 by damorim-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stddef.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, const char *s2);
-int	fill_stash(int fd, char **stash);
+int		fill_stash(int fd, char **stash);
 char	*extract_line(char *stash);
 char	*next_stash(char *stash);
-
-
 #endif
